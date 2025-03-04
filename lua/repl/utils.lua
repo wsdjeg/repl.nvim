ca00e1f30160640d057b6d33566f548fcf34fb17
Eric Wong <wsdjeg@outlook.com>
2025-03-04 21:08:34 +0800

    init version

---
+++ lua/repl/utils.lua
@@ -0,0 +1,31 @@
+local M = {}
+
+function M.setlocalopt(buf, win, opts)
+  local getinfo
+  if vim.api.nvim_get_option_info2 then
+    getinfo = function(o)
+      return vim.api.nvim_get_option_info2(o, {})
+    end
+  else
+    getinfo = function(o)
+      return vim.api.nvim_get_option_info(o)
+    end
+  end
+  for o, value in pairs(opts) do
+    local info = getinfo(o)
+    if info.scope == 'win' then
+      vim.api.nvim_set_option_value(o, value, {
+        win = win,
+      })
+    elseif info.scope == 'buf' then
+      vim.api.nvim_set_option_value(o, value, {
+        buf = buf,
+      })
+    end
+  end
+end
+
+function M.trim(str)
+  return str:match('^%s*(.-)%s*$')
+end
+return M
